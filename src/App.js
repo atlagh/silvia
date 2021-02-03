27a8cc254439af28c8ffb5af22e103ed21b525a6
amine <atlagh.amine@gmail.com>
2021-02-03 18:58:06 +0100

    start footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 class App extends React.Component {
   constructor(){
-
     super();
   }
   render(){
     return(
+    	<div>
           <Navbar />
+          <Footer />
+        </div>
        
       )
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+
+	return(
+		<footer>
+			<div className='FootWrap'>
+				hey
+			</div>
+			<div>
+				HI
+			</div>
+		</footer>
+
+		)
+}
+
+export default Footer;
